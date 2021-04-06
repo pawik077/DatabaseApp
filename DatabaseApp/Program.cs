30f89592ed5d4c05ecf6f9003191a0b9372c40be
pawik077 <pawik.r.s.077@gmail.com>
2021-04-07 01:18:09 +0200

    Dodano wpis czasu pobrania danych do bazy, #nocna

--- DatabaseApp/Program.cs
+++ DatabaseApp/Program.cs
@@ -12,6 +12,7 @@ namespace DatabaseApp {
     public string city { set; get; }
     public string weather { set; get; }
     public float temperature { set; get; }
+    public DateTime time { set; get; }
   }
   public class WeatherDb : DbContext {
 
@@ -21,14 +22,15 @@ namespace DatabaseApp {
   class Program {
     const string apiKey = "a4d7d088521a6fd7b655eec8dd00e43a";
     static void Main(string[] args) {
-      
+
       Console.Write("Enter city: ");
       string city = Console.ReadLine();
       string url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&&appid=" + apiKey;
       RestClient client = new RestClient(url);
       RestRequest request = new RestRequest(Method.GET);
       IRestResponse response = client.Execute(request);
-      if(!response.IsSuccessful) {
+      if (!response.IsSuccessful)
+      {
         Console.Error.WriteLine("Unknown city!!");
         return;
       }
@@ -37,14 +39,15 @@ namespace DatabaseApp {
       //Console.WriteLine("Weather: " + json.SelectToken("weather[0].main"));
       //Console.WriteLine("Current temperature: " + json.SelectToken("main.temp") + "\u00B0C");
       var context = new WeatherDb();
-      var newData = new WeatherData {  city = city, weather = json.SelectToken("weather[0].main").ToString(), temperature = (float)json.SelectToken("main.temp")  };
-      
+      var newData = new WeatherData { city = city, weather = json.SelectToken("weather[0].main").ToString(), temperature = (float)json.SelectToken("main.temp"), time = DateTime.Now };
+
       context.Weather.Add(newData);
       context.SaveChanges();
       //var weatherList = context.Weather.SqlQuery("SELECT * FROM Weather").ToList();
       var weatherList = (from w in context.Weather select w).ToList();
-      foreach (var w in weatherList) {
-        Console.WriteLine("City: " + w.city + "\tWeather: " + w.weather + "\tTemperature: " + w.temperature);
+      foreach (var w in weatherList)
+      {
+        Console.WriteLine("City: " + w.city + "\tWeather: " + w.weather + "\tTemperature: " + w.temperature + "\tTime: " + w.time);
       }
       Console.Read();
     }
