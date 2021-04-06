59e0c9b8a0fe2d322690deb9557560f7b73e3092
pawik077 <pawik.r.s.077@gmail.com>
2021-04-07 01:07:57 +0200

    Migracja wsteczna na .NET Framework, na nocnej

--- DatabaseApp/Program.cs
+++ DatabaseApp/Program.cs
@@ -1,24 +1,21 @@
 ﻿using System;
 using Newtonsoft.Json.Linq;
 using RestSharp;
-using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
 using System.Linq;
 using System.ComponentModel.DataAnnotations;
 
 
 namespace DatabaseApp {
     public class WeatherData {
-      [Key]
-      public int id { get; set; }
-      public string city { set; get; }
-      public string weather { set; get; }
-      public float temperature { set; get; }
+    public int ID { get; set; }
+    public string city { set; get; }
+    public string weather { set; get; }
+    public float temperature { set; get; }
   }
   public class WeatherDb : DbContext {
-    //public WeatherDb(DbContextOptions<WeatherDb> options) : base(options) { }
+
     public virtual DbSet<WeatherData> Weather { get; set; }
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-    => options.UseSqlite("DataSource=app.db");
   }
 
   class Program {
@@ -28,8 +25,8 @@ namespace DatabaseApp {
       Console.Write("Enter city: ");
       string city = Console.ReadLine();
       string url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&&appid=" + apiKey;
-      RestClient client = new(url);
-      RestRequest request = new(Method.GET);
+      RestClient client = new RestClient(url);
+      RestRequest request = new RestRequest(Method.GET);
       IRestResponse response = client.Execute(request);
       if(!response.IsSuccessful) {
         Console.Error.WriteLine("Unknown city!!");
@@ -42,12 +39,14 @@ namespace DatabaseApp {
       var context = new WeatherDb();
       var newData = new WeatherData {  city = city, weather = json.SelectToken("weather[0].main").ToString(), temperature = (float)json.SelectToken("main.temp")  };
       
-      context.Add(newData);
+      context.Weather.Add(newData);
       context.SaveChanges();
-      var weatherList = context.Weather.FromSqlRaw("SELECT * FROM Weather").ToList<WeatherData>();
+      //var weatherList = context.Weather.SqlQuery("SELECT * FROM Weather").ToList();
+      var weatherList = (from w in context.Weather select w).ToList();
       foreach (var w in weatherList) {
         Console.WriteLine("City: " + w.city + "\tWeather: " + w.weather + "\tTemperature: " + w.temperature);
       }
+      Console.Read();
     }
   }
 }
