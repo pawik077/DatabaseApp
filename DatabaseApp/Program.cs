ad0de0a59331a2a84fe8ca0269cf4ea8215a648b
pawik077 <pawik.r.s.077@gmail.com>
2021-04-14 14:34:43 +0200

    Dodano obsługę wątków, lab3

--- DatabaseApp/Program.cs
+++ DatabaseApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using Newtonsoft.Json.Linq;
 using RestSharp;
 using System.Data.Entity;
@@ -18,79 +19,81 @@ namespace DatabaseApp {
 
     public virtual DbSet<WeatherData> Weather { get; set; }
   }
-
+        
   class Program {
     const string apiKey = "a4d7d088521a6fd7b655eec8dd00e43a";
-    static void Main(string[] args) {
-      int op;
-      var context = new WeatherDb();
-      do
-      {
-        Console.WriteLine("Option 1 - Add to database");
-        Console.WriteLine("Option 2 - List all database entries");
-        Console.WriteLine("Option 3 - List all database entries for selected city");
-        Console.WriteLine("Option 0 - Exit");
-        Console.Write("Option: ");
-        op = int.Parse(Console.ReadLine());
-        switch (op)
+        static void ApiDownload(string city, ref JToken json)
         {
-          case 1:
+            Thread.Sleep(500);
+            string url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&&appid=" + apiKey;
+            RestClient client = new RestClient(url);
+            RestRequest request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            if (!response.IsSuccessful)
             {
-              Console.Write("Enter city: ");
-              string city = Console.ReadLine();
-              string url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&&appid=" + apiKey;
-              RestClient client = new RestClient(url);
-              RestRequest request = new RestRequest(Method.GET);
-              IRestResponse response = client.Execute(request);
-              if (!response.IsSuccessful)
-              {
-                Console.Error.WriteLine("Unknown city!!");
-                return;
-              }
-              JToken json = JToken.Parse(response.Content);
-              //Console.WriteLine("Current weather in " + city + ":");
-              //Console.WriteLine("Weather: " + json.SelectToken("weather[0].main"));
-              //Console.WriteLine("Current temperature: " + json.SelectToken("main.temp") + "\u00B0C");
-              var newData = new WeatherData { city = city, weather = json.SelectToken("weather[0].main").ToString(), temperature = (float)json.SelectToken("main.temp"), time = DateTime.Now };
-
-              context.Weather.Add(newData);
-              context.SaveChanges();
-              break;
+                throw new InvalidOperationException("Unknown city!!");
             }
-          case 2:
-            {
-              var weatherList = (from w in context.Weather select w).ToList();
-              foreach (var w in weatherList)
-              {
-                Console.WriteLine("City: " + w.city + "\tWeather: " + w.weather + "\tTemperature: " + w.temperature + "\tTime: " + w.time);
-              }
-              break;
-            }
-          case 3:
-            {
-              Console.Write("Enter city: ");
-              string city = Console.ReadLine();
-              var weatherList = (from w in context.Weather where w.city == city select w).ToList();
-              foreach (var w in weatherList)
-              {
-                Console.WriteLine("City: " + w.city + "\tWeather: " + w.weather + "\tTemperature: " + w.temperature + "\tTime: " + w.time);
-              }
-              break;
-            }
-          case 0:
-            {
-              break;
-            }
-          default:
-            {
-              Console.WriteLine("Wrong option!!");
-              op = -1;
-              break;
-            }
+            json = JToken.Parse(response.Content);
         }
-      } while (op != 0);
+        static void Main(string[] args) {
+          int op;
+          var context = new WeatherDb();
+          do
+          {
+            Console.WriteLine("Option 1 - Add to database");
+            Console.WriteLine("Option 2 - List all database entries");
+            Console.WriteLine("Option 3 - List all database entries for selected city");
+            Console.WriteLine("Option 0 - Exit");
+            Console.Write("Option: ");
+            op = int.Parse(Console.ReadLine());
+            switch (op)
+            {
+              case 1:
+                {
+                  Console.Write("Enter city: ");
+                  string city = Console.ReadLine();
+                  JToken json = null;
+                  Thread thread = new Thread(() =>  ApiDownload(city, ref json));
+                  thread.Start();
+                  thread.Join();
+                    var newData = new WeatherData { city = city, weather = json.SelectToken("weather[0].main").ToString(), temperature = (float)json.SelectToken("main.temp"), time = DateTime.Now };
 
-      //var weatherList = context.Weather.SqlQuery("SELECT * FROM Weather").ToList();
+                    context.Weather.Add(newData);
+                    context.SaveChanges();
+                    break;
+                  }
+                case 2:
+                  {
+                    var weatherList = (from w in context.Weather select w).ToList();
+                    foreach (var w in weatherList)
+                    {
+                      Console.WriteLine("City: " + w.city + "\tWeather: " + w.weather + "\tTemperature: " + w.temperature + "\tTime: " + w.time);
+                    }
+                    break;
+                  }
+                case 3:
+                  {
+                    Console.Write("Enter city: ");
+                    string city = Console.ReadLine();
+                    var weatherList = (from w in context.Weather where w.city == city select w).ToList();
+                    foreach (var w in weatherList)
+                    {
+                      Console.WriteLine("City: " + w.city + "\tWeather: " + w.weather + "\tTemperature: " + w.temperature + "\tTime: " + w.time);
+                    }
+                    break;
+                  }
+                case 0:
+                  {
+                    break;
+                  }
+                default:
+                  {
+                    Console.WriteLine("Wrong option!!");
+                    op = -1;
+                    break;
+                  }
+              }
+      } while (op != 0);
     }
   }
 }
